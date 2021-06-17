ac886501b2394bbc85c5c84be4836238f1d6bde5
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-06-17 12:40:21 -0400

    Adding Refresh button to fetch tours back

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -9,33 +9,43 @@ function App() {
   const [tours, setTours] = useState([]);
 
   const removeTour = (id) => {
-    const newTours = tours.filter((tour) => tour.id !== id)
-    setTours(newTours)
-  }
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
 
   const fetchTours = async () => {
-    setLoading(true);   // set extra pre cautious
+    setLoading(true); // set extra pre cautious
     try {
       const response = await fetch(url);
       const tours = await response.json();
-      setLoading(false)
-      setTours(tours)
-      console.log(tours)
+      setLoading(false);
+      setTours(tours);
+      console.log(tours);
     } catch (error) {
-      setLoading(false)
-      console.log(error)
+      setLoading(false);
+      console.log(error);
     }
-   
-  }
+  };
 
   useEffect(() => {
     fetchTours();
-  }, [])
+  }, []);
 
   if (loading) {
     return (
       <main>
         <Loading />
+      </main>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div>
+          <h2>No Tours Left!</h2>
+          <button className='btn' onClick={fetchTours}>Refresh!</button>
+        </div>
       </main>
     );
   }
