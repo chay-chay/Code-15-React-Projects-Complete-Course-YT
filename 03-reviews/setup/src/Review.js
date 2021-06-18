98bc172ef05d5afcfb322693f31f61a152a6877e
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-06-18 10:58:28 -0400

    Adding condition for randomPerson

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -6,17 +6,32 @@ const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
 
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0
+    }
+    if (number < 0) {
+      return people.length -1
+    }
+    return number
+  }
+
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    setIndex(randomNumber)
+  }
+
   const nextPerson = () => {
     setIndex((index) => {
       let newIndex = index + 1;
-      return newIndex
+      return checkNumber(newIndex)
     })
   }
 
   const prevPerson = () => {
     setIndex((index) => {
       let newIndex = index + 1;
-      return newIndex
+      return checkNumber(newIndex)
     })
   }
 
@@ -39,7 +54,7 @@ const Review = () => {
           <FaChevronRight />
         </button>
       </div>
-      <button className="random-btn">surprise me!</button>
+      <button className="random-btn" onClick={randomPerson}>surprise me!</button>
     </article>
   );
 };
