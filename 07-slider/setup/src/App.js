c31ac54e477ab6ed6e4c97ff0e6a33e1feef2b45
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-06-28 12:35:43 -0400

    Adding buttons

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -19,10 +19,20 @@ function App() {
           const { id, image, name, title, quote } = person;
           return (
             <article key={id}>
-              <img src={image} alt={name} />
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
             </article>
           );
         })}
+        <button className='prev'>
+          <FiChevronLeft />
+        </button>
+        <button className='next'>
+          <FiChevronRight />
+        </button>
       </div>
     </section>
   );
