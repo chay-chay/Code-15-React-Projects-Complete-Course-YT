89b7be9983f94e6ead59dbf7e17aa4bfbf8f950d
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-07-06 12:28:40 -0400

    Set condition for next button

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -12,7 +12,10 @@ function App() {
     if (index < 0) {
       setIndex(lastIndex)
     }
-  })
+    if (index > lastIndex) {
+      setIndex(0)
+    }
+  }, [index, people])
 
   return (
     <section className="section">
