9cfe55d850c9efdbf5bdf8e44911ab9689a56394
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-07-07 22:57:41 -0400

    Added form for adding paragraph

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,24 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello");
+  };
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>Tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onClick={handleSubmit}>
+        <label htmlFor="amount">Paragraph: </label>
+        <input type="number" name="amount" id="amount" value={count}
+          onChange={(e) => setCount(e.target.value)}></input>
+        <button type='submit' className='btn'>Generate</button>
+      </form>
+    </section>
+  );
 }
 
 export default App;
