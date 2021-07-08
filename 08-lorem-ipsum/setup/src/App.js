b495293e52bc669599f1f9265be417a7b2f6da43
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-07-07 23:06:31 -0400

    Added handleSubmit and fetch data

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -6,7 +6,7 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello");
+    setText(data)
   };
   return (
     <section className="section-center">
@@ -17,6 +17,11 @@ function App() {
           onChange={(e) => setCount(e.target.value)}></input>
         <button type='submit' className='btn'>Generate</button>
       </form>
+      <article className='lorem-text'>
+        {text.map((item, index) => {
+          return <p key={index}>{ item}</p>
+        })}
+      </article>
     </section>
   );
 }
