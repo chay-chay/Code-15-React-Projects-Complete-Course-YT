444cb45028e7599b49b82825a6bcfda20a85fa3b
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-07-23 23:32:38 -0400

    Change onSubmit to onClick

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -6,29 +6,32 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // type of input is string - that's why we have to change to integer
-    let amount = parseInt(count)
-    
+    let amount = parseInt(count);
     if (count <= 0) {
-      amount = 1
+      amount = 1;
     }
     if (count > 8) {
-      amount = 8
+      amount = 8;
     }
-    setText(data.slice(0, amount))
+    setText(data.slice(0, amount));
   };
   return (
     <section className="section-center">
-      <h3>Tired of boring lorem ipsum?</h3>
+      <h3>tired of boring lorem ipsum?</h3>
       <form className="lorem-form" onClick={handleSubmit}>
-        <label htmlFor="amount">Paragraph: </label>
-        <input type="number" name="amount" id="amount" value={count}
-          onChange={(e) => setCount(e.target.value)}></input>
-        <button type='submit' className='btn'>Generate</button>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type='submit' className="btn">Generate</button>
       </form>
-      <article className='lorem-text'>
+      <article className="lorem-text">
         {text.map((item, index) => {
-          return <p key={index}>{ item}</p>
+          return <p key={index}>{item}</p>;
         })}
       </article>
     </section>
@@ -36,3 +39,43 @@ function App() {
 }
 
 export default App;
+
+
+
+// import React, { useState } from "react";
+// import data from "./data";
+// function App() {
+//   const [count, setCount] = useState(0);
+//   const [text, setText] = useState([]);
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     // type of input is string - that's why we have to change to integer
+//     let amount = parseInt(count);
+//     if (count <= 0) {
+//       amount = 1;
+//     }
+//     if (count > 8) {
+//       amount = 8;
+//     }
+//     setText(data.slice(0, amount));
+//   };
+//   return (
+//     <section className="section-center">
+//       <h3>Tired of boring lorem ipsum?</h3>
+//       <form className="lorem-form" onClick={handleSubmit}>
+//         <label htmlFor="amount">Paragraph: </label>
+//         <input type="number" name="amount" id="amount" value={count}
+//           onChange={(e) => setCount(e.target.value)}></input>
+//         <button type='submit' className='btn'>Generate</button>
+//       </form>
+//       <article className='lorem-text'>
+//         {text.map((item, index) => {
+//           return <p key={index}>{ item}</p>
+//         })}
+//       </article>
+//     </section>
+//   );
+// }
+
+// export default App;
