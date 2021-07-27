170de62e4d41ff2da8a3fd21cf111c358c09a046
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-07-26 23:51:34 -0400

    Join rgb to bcg

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = ({rgb, weight, index}) => {
+const SingleColor = ({ rgb, weight, index }) => {
+  const [alert, setAlert] = useState(false)
+  const bcg = rgb.join(',')
+  console.log(rgb);
+  console.log(bcg);
   return <h4>single color</h4>
 }
 
