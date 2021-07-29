42e7e19a73ccf4cd581cb64a9874504ec94e2bad
Chayanit <chayanit.chaisri@gmail.com>
2021-07-29 11:14:34 -0400

    Added condition for index > 10 && change to ligh color

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -2,16 +2,16 @@ import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
 const SingleColor = ({ rgb, weight, index, hexColor }) => {
-  console.log(hexColor)
+  console.log(index)
   const [alert, setAlert] = useState(false)
   const bcg = rgb.join(',')
   // we can use the function or use library
   const hex = rgbToHex(...rgb)
-  console.log(rgb);
-  console.log(bcg);
-  return <article className={`color`} style={{backgroundColor: `rgb(${bcg})`}}>
+  // console.log(rgb);
+  // console.log(bcg);
+  return <article className={`color ${index > 10 && 'color-light'}`} style={{backgroundColor: `rgb(${bcg})`}}>
   <p className="percenr-value">{weight}%</p>
-  <p className="color-value">{hexColor}</p>
+  <p className="color-value"># {hexColor}</p>
   </article>
 }
 
