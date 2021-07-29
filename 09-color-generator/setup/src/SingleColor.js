ed8ab333917835edc574e63026447b01982012e6
Chayanit <chayanit.chaisri@gmail.com>
2021-07-28 20:39:42 -0400

    Added hex & use colors from library

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -2,14 +2,16 @@ import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
 const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  console.log(hexColor)
   const [alert, setAlert] = useState(false)
   const bcg = rgb.join(',')
+  // we can use the function or use library
   const hex = rgbToHex(...rgb)
   console.log(rgb);
   console.log(bcg);
   return <article className={`color`} style={{backgroundColor: `rgb(${bcg})`}}>
   <p className="percenr-value">{weight}%</p>
-  <p className="color-value">{hex}</p>
+  <p className="color-value">{hexColor}</p>
   </article>
 }
 
