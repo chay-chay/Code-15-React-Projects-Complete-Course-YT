cd59d9bb959214292d131c39421010b05cf2377b
Chayanit <chayanit.chaisri@gmail.com>
2021-08-01 21:06:22 -0400

    Added List and const Name, List, isEditing, editingID, alert

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -1,9 +1,19 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isediting, setEditing] = useState(false);
+  const [editingID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+  return (
+    <section className="section-center">
+      <div className="grocery-container"></div>
+      <List />
+    </section>
+  );
 }
 
-export default App
+export default App;
