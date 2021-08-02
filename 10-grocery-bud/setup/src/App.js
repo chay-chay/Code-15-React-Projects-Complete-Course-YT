7458f9b843e32a5de60492ee43903e8db8560c8d
Chayanit <chayanit.chaisri@gmail.com>
2021-08-02 07:30:10 -0400

    Added input form

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -5,13 +5,29 @@ import Alert from "./Alert";
 function App() {
   const [name, setName] = useState("");
   const [list, setList] = useState([]);
-  const [isediting, setEditing] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
   const [editingID, setEditID] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log("hi")
+  }
   return (
     <section className="section-center">
+    <form className='grocery-form' onSubmit={handleSubmit}>
+      {alert.show && <Alert />}
+      <h3>grocery bud</h3>
+      <div className='form-control'>
+        <input type='texrt' />
+        <button type='submit' className='submit-btn'>
+          {isEditing? 'edit' : 'submit'}
+        </button>
+      </div>
+    </form>
       <div className="grocery-container"></div>
       <List />
+      <button className='clear-btn'>clear items</button>
+     
     </section>
   );
 }
