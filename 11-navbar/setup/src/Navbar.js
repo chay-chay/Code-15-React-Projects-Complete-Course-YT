87963cb57dc46bdda482420f09a1891ba39c9ea5
Chayanit <chayanit.chaisri@gmail.com>
2021-08-11 15:38:14 -0400

    Import social instead of hard coding

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -26,21 +26,16 @@ const Navbar = () => {
           </ul>
         </div>
         <ul className="social-icons">
-          <li>
-            <a href="https://www.twitter.com">
-              <FaTwitter />
+          {social.map((socialIcon) => {
+            const {id, url,icon} = socialIcon
+            return(
+              <li key={id}>
+            <a href="url">
+              {icon}
             </a>
           </li>
-          <li>
-            <a href="https://www.twitter.com">
-              <FaTwitter />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.twitter.com">
-              <FaTwitter />
-            </a>
-          </li>
+            )
+          })}
         </ul>
       </div>
     </nav>
