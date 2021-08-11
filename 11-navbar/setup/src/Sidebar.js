544aa0279911a9ccc425427893910fbad760dd34
Chayanit <chayanit.chaisri@gmail.com>
2021-08-11 15:34:30 -0400

    Import links instead of hard coding

---
+++ 11-navbar/setup/src/Sidebar.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <div className="links-container show-container">
+        <ul className="links">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+          <li>
+            <a href="#">Product</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
