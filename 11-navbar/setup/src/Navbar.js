8c5e0036019149fec84b807efb6c982a7a694081
Chayanit <chayanit.chaisri@gmail.com>
2021-08-16 23:37:42 -0400

    Set up onClick showLinks

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -4,16 +4,20 @@ import { links, social } from "./data";
 import logo from "./logo.svg";
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false)
+
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="logo" />
-          <button className="nav-toggle">
+          <button className="nav-toggle"
+          onClick={() => setShowLinks(!showLinks)}>
             <FaBars />
           </button>
         </div>
-        <div className="links-container show-container">
+        {showLinks && (
+          <div className="links-container show-container">
           <ul className="links">
             {links.map((link) => {
               const { id, url, text } = link;
@@ -25,16 +29,15 @@ const Navbar = () => {
             })}
           </ul>
         </div>
+        )}
         <ul className="social-icons">
           {social.map((socialIcon) => {
-            const {id, url,icon} = socialIcon
-            return(
+            const { id, url, icon } = socialIcon;
+            return (
               <li key={id}>
-            <a href="url">
-              {icon}
-            </a>
-          </li>
-            )
+                <a href={url}>{icon}</a>
+              </li>
+            );
           })}
         </ul>
       </div>
