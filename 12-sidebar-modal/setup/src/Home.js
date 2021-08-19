a534701d8d0afc854f088ae389c593d5c46fac3d
Chayanit <chayanit.chaisri@gmail.com>
2021-08-18 23:30:49 -0400

    Added modal

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
       <button className="sidebar-toggle">
         <FaBars />
       </button>
-      
+      <button className='btn'>Show Button</button>
     </main>
   );
 };
